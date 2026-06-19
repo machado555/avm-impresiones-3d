8b46d0635a58b95dee70cde97fb3b560459b3bad
machado555 <rmachadoedenor@gmail.com>
2026-06-19 08:51:45 -0300

    Update products.ts
    varias categorías mas

--- types/products.ts
+++ types/products.ts
@@ -1,6 +1,10 @@
 export type ProductFilters = {
   category?: string;
   categorySlug?: string;
+  tagSlug?: string;
+  materialSlug?: string;
+  color?: string;
+  size?: string;
   minPrice?: number;
   maxPrice?: number;
   search?: string;
